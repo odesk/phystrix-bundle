b5feede8f674e65437dd4c384acc0cd43ec8fe45
Dmitry Semenov <mxnr@elance-odesk.com>
2015-05-07 22:52:07 +0700

    Codestyles fixes

--- Tests/DataCollector/RequestLogDataCollectorTest.php
+++ Tests/DataCollector/RequestLogDataCollectorTest.php
@@ -1,21 +1,21 @@
 <?php
 /**
-     * This file is a part of the Phystrix Bundle.
-     *
-     * Copyright 2013-2015 oDesk Corporation. All Rights Reserved.
-     *
-     * This file is licensed under the Apache License, Version 2.0 (the "License");
-     * you may not use this file except in compliance with the License.
-     * You may obtain a copy of the License at
-     *
-     * http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
+ * This file is a part of the Phystrix Bundle.
+ *
+ * Copyright 2013-2015 oDesk Corporation. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 namespace Odesk\Bundle\PhystrixBundle\Tests\DataCollector;
 
@@ -52,8 +52,8 @@ class RequestLogDataCollectorTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @param string $name
-     * @param integer $executionTime
+     * @param string   $name
+     * @param integer  $executionTime
      * @param string[] $executionEvents
      *
      * @return \PHPUnit_Framework_MockObject_MockObject|\Odesk\Phystrix\AbstractCommand
